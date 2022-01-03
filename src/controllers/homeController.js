267045cd449afa6f09194f0e77d0dbd497c08731
SangTran <sang.tran@adurolife.com>
2022-01-03 17:37:50 +0700

    complete CRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,7 @@ import pool from '../configs/connectDB';
 
 let getHomePage = async (req, res) => {
   const [rows, fields] = await pool.execute(`SELECT * from users`);
-  
+
   return res.render('index.ejs', {dataUser: rows, test: 'abc'});
 }
 
@@ -21,8 +21,33 @@ let createNewUser = async (req, res) => {
   return res.redirect('/');
 }
 
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute('Delete from users where id = ?', [userId])
+
+  return res.redirect('/');
+}
+
+let editUser = async (req, res) => {
+  const userId = req.params.id;
+  const [user, fields] = await pool.execute(`SELECT * from users where id = ? `, [userId]);
+
+  return res.render('update.ejs', {dataUser: user[0]})
+}
+
+let postUpdateUser = async (req, res) => {
+  let { id, firstName, lastName, email, address } = req.body;
+  await pool.execute(`Update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?`, 
+    [firstName, lastName, email, address, id])
+
+  return res.redirect('/')
+}
+
 module.exports = {
   getHomePage,
   getDetailPage,
-  createNewUser
+  createNewUser,
+  deleteUser,
+  editUser,
+  postUpdateUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -7,6 +7,10 @@ const initWebRoutes = (app) => {
   route.get('/', homeController.getHomePage)
   route.get('/detail/user/:userId', homeController.getDetailPage)
   route.post('/create-new-user', homeController.createNewUser)
+  route.post('/delete-user', homeController.deleteUser)
+  route.get('/edit-user/:id', homeController.editUser)
+  route.post('/post-update-user', homeController.postUpdateUser)
+
   route.get('/about', (req, res) => {
     res.send('<h1>I\'m Hani Kawaiiiii</h1>')
   })
