f0346740d66823505a0ffdd3a03b70c5db8a4d70
SangTran <sang.tran@adurolife.com>
2022-01-05 17:39:40 +0700

    upload single file using multer package

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,47 +1,90 @@
-import pool from '../configs/connectDB';
+import pool from "../configs/connectDB";
+import multer from "multer";
 
 let getHomePage = async (req, res) => {
   const [rows, fields] = await pool.execute(`SELECT * from users`);
 
-  return res.render('index.ejs', {dataUser: rows, test: 'abc'});
-}
+  return res.render("index.ejs", { dataUser: rows, test: "abc" });
+};
 
 let getDetailPage = async (req, res) => {
   const userId = req.params.userId;
-  const [user, fields] = await pool.execute(`SELECT * from users where id = ? `, [userId]);
+  const [user, fields] = await pool.execute(
+    `SELECT * from users where id = ? `,
+    [userId]
+  );
 
   return res.send(JSON.stringify(user));
-}
+};
 
 let createNewUser = async (req, res) => {
+  console.log(">> body: ", req.body);
   let { firstName, lastName, email, address } = req.body;
-  await pool.execute(`Insert into users(firstName, lastName, email, address) values (? , ? , ? ,? )`,
-   [firstName, lastName, email, address]);
+  await pool.execute(
+    `Insert into users(firstName, lastName, email, address) values (? , ? , ? ,? )`,
+    [firstName, lastName, email, address]
+  );
 
-  return res.redirect('/');
-}
+  return res.redirect("/");
+};
 
 let deleteUser = async (req, res) => {
   let userId = req.body.userId;
-  await pool.execute('Delete from users where id = ?', [userId])
+  await pool.execute("Delete from users where id = ?", [userId]);
 
-  return res.redirect('/');
-}
+  return res.redirect("/");
+};
 
 let editUser = async (req, res) => {
   const userId = req.params.id;
-  const [user, fields] = await pool.execute(`SELECT * from users where id = ? `, [userId]);
+  const [user, fields] = await pool.execute(
+    `SELECT * from users where id = ? `,
+    [userId]
+  );
 
-  return res.render('update.ejs', {dataUser: user[0]})
-}
+  return res.render("update.ejs", { dataUser: user[0] });
+};
 
 let postUpdateUser = async (req, res) => {
   let { id, firstName, lastName, email, address } = req.body;
-  await pool.execute(`Update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?`, 
-    [firstName, lastName, email, address, id])
+  await pool.execute(
+    `Update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?`,
+    [firstName, lastName, email, address, id]
+  );
 
-  return res.redirect('/')
-}
+  return res.redirect("/");
+};
+
+let getUploadFilePage = (req, res) => {
+  return res.render("upload.ejs");
+};
+
+const upload = multer().single("profile_pic");
+
+let handleUploadFile = async (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+  upload(req, res, function (err) {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      return res.send("Please select an image to upload");
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
+
+    // Display uploaded image for user validation
+    res.send(
+      `You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="/upload-file">Upload another image</a>`
+    );
+  });
+};
+
+let handleUploadMultipleFiles = (req, res) => {};
 
 module.exports = {
   getHomePage,
@@ -49,5 +92,8 @@ module.exports = {
   createNewUser,
   deleteUser,
   editUser,
-  postUpdateUser
-}
+  postUpdateUser,
+  getUploadFilePage,
+  handleUploadFile,
+  handleUploadMultipleFiles,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,21 +1,57 @@
-import express from 'express';
-import * as homeController from '../controllers/homeController';
+import express from "express";
+import * as homeController from "../controllers/homeController";
+import multer from "multer";
+import path from "path";
+
+var appRoot = require("app-root-path");
 
 const route = express.Router();
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, appRoot + "/src/public/images/");
+  },
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
 const initWebRoutes = (app) => {
-  route.get('/', homeController.getHomePage)
-  route.get('/detail/user/:userId', homeController.getDetailPage)
-  route.post('/create-new-user', homeController.createNewUser)
-  route.post('/delete-user', homeController.deleteUser)
-  route.get('/edit-user/:id', homeController.editUser)
-  route.post('/post-update-user', homeController.postUpdateUser)
+  route.get("/", homeController.getHomePage);
+  route.get("/detail/user/:userId", homeController.getDetailPage);
+  route.post("/create-new-user", homeController.createNewUser);
+  route.post("/delete-user", homeController.deleteUser);
+  route.get("/edit-user/:id", homeController.editUser);
+  route.post("/post-update-user", homeController.postUpdateUser);
 
-  route.get('/about', (req, res) => {
-    res.send('<h1>I\'m Hani Kawaiiiii</h1>')
-  })
+  //Upload files
+  route.get("/upload-file", homeController.getUploadFilePage);
+  route.post(
+    "/upload-profile-pic",
+    upload.single("profile_pic"), // middleware
+    homeController.handleUploadFile
+  );
+  route.post(
+    "/upload-multiple-images",
+    homeController.handleUploadMultipleFiles
+  );
 
-  app.use('/', route)
-}
+  app.use("/", route);
+};
 
 export default initWebRoutes;
