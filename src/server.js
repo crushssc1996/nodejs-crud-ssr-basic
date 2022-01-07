48db02a60ac2c8e918aeeaff8cb0c92c5e353695
SangTran <sang.tran@adurolife.com>
2022-01-07 16:11:55 +0700

    create a few of middleware

--- src/server.js
+++ src/server.js
@@ -1,18 +1,31 @@
 // const express = require('express');
-import express from 'express';
-import configViewEngine from './configs/viewEngine';
-import initWebRoutes from './routes/web';
-import initAPIRoutes from './routes/api';
-import connection from './configs/connectDB';
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+import initAPIRoutes from "./routes/api";
+import connection from "./configs/connectDB";
+import morgan from "morgan";
 
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
-const port  = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-//setup payload in body of request methods and send to server 
+// Middlware -> Code run top to bottom,
+// if Valid, call Next to go next.
+app.use((req, res, next) => {
+  console.log(">> run into middleware ");
+  console.log(req.method);
+  next();
+});
+
+// setup write logs middleware
+app.use(morgan("combined"));
+
+//setup payload in body of request methods and send to server
 //vice versa
-app.use(express.urlencoded({ extended: true}));
+//Middleware to config body data of req
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // setup view engine
@@ -24,6 +37,11 @@ initWebRoutes(app);
 // setup api routes
 initAPIRoutes(app);
 
+// Middleware to return 404 page
+app.use((req, res) => {
+  return res.render("404page.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
-})
+});
